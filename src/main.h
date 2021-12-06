73c75f7caeb0d8f0393641f3185e3583cfb8ab87
ambiennt <ahook701@gmail.com>
2021-12-05 17:13:38 -0800

    initial commit

---
+++ src/main.h
@@ -0,0 +1,23 @@
+#pragma once
+
+#include <base/log.h>
+#include <hook.h>
+#include <base/base.h>
+#include <yaml.h>
+#include <Actor/Player.h>
+#include <Actor/ActorDamageSource.h>
+#include <Actor/Attribute.h>
+#include <Level/Level.h>
+#include <Packet/PlaySoundPacket.h>
+#include <Packet/TextPacket.h>
+
+inline struct Settings {
+
+    bool useResourcePackGlyphs = false;
+
+    template <typename IO> static inline bool io(IO f, Settings &settings, YAML::Node &node) {
+        return f(settings.useResourcePackGlyphs, node["useResourcePackGlyphs"]);
+    }
+} settings;
+
+DEF_LOGGER("BowDing");
